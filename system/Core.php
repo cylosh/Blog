287a23f516da52cbc996c5f52558c33084eabe27
AI Cylosh <ionuthrive@yahoo.com>
2017-01-26 19:23:48 +0200

    Update framework Core

--- system/Core.php
+++ system/Core.php
@@ -1,16 +1,18 @@
 <?php
 
 /**
- * Core: Enables access to core methods of the application
+ * Core: Enables access to core methods and properties of the framework
  *
  * @author : Cylosh
  * @link : https://www.cylo.ro
  * @copyright : https://opensource.org/licenses/MIT
-
+ *
  * @version 1.0: 23 January 2017
  * @version 2: 25 January 2017
  *			added link resolver
-*/
+ * @version 3: 26 January 2017
+ *			added cache capabilities for HTML content
+**/
 namespace system;
 
 /*
@@ -81,86 +83,15 @@ class Core{
 		
 	// }
 	
-	// include Page template
-	protected function HtmlView(array $location){
-
-		$path = dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.implode(DIRECTORY_SEPARATOR, $location).'.php';
-
-		if(!file_exists($path)){
-			
-			#header("Location: ".SITE_URI."/error/503");
-			exit;
-		}
-		
-		$this->HTMLPath = $path;
-		
-		return true;
-	}
 	
-    protected function authorise(){
-        echo "content ".$this->var;
-                var_dump($_SESSION);
-		/* if($this->permission > $permissions){
-		
-			// si in plus daca $this->permission $this->userInput e post sau update atunci $this->getowner si s
-		} */
-    }
-    
-    public function presentation(){
-		
-		$content = '';
-		
-		// get content shipping method
-		if(isset($_GET['json']))
-			$_SESSION['shipment'] = 'json';
-		elseif(isset($_GET['xml']))
-			$_SESSION['shipment'] = 'xml';
-		elseif(!isset($_SESSION['shipment']) OR isset($_GET['html']))
-			$_SESSION['shipment'] = 'html';
-		
-		if($_SESSION['shipment'] != 'html')
-			
-			header("Content-Type: application/{$_SESSION['shipment']}; charset=utf-8");
-		
-		else{ // ENABLE COMPRESSION for HTML
-			
-			ob_start("ob_gzhandler");
-			include $this->HTMLPath;
-			$content = $this->linkBridge(ob_get_clean());
-			
-			// Determine supported compression method
-			$gzip = strstr($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip');
-			$deflate = strstr($_SERVER['HTTP_ACCEPT_ENCODING'], 'deflate');
-			
-			// Determine used compression method
-			$encoding = $gzip ? 'gzip' : ($deflate ? 'deflate' : false);
-
-			if($encoding){
-				$content = gzencode(trim( preg_replace( '/\s+/', ' ', $content ) ), 9);
-				$offset = 60 * 60 * 24;
-				$expire = "Expires: " . gmdate("D, d M Y H:i:s", time() + $offset) . " GMT";
-				header('Content-Encoding: gzip');
-				header("content-type: text/html; charset=UTF-8");
-				header( $expire );
-				header('Content-Length: ' . strlen( $content ) );
-				header('Vary: Accept-Encoding');
-			}
-		}
-		
-		print $content;
-	}
-	
-	/*
-	* linkBridge: Link resolver
-	* @param string $html
-	*		Page HTML to fix links for css, images, javascript
-	*		
-	* @return string
-	* 
-	* @author : Cylosh
-	* @link : https://www.cylo.ro
-	* @copyright : https://opensource.org/licenses/MIT
-	 */
+	/**
+	 * linkBridge: Link resolver
+	 * 
+	 * @param string $html
+	 *		Page HTML to fix links for css, images, javascript
+	 *		
+	 * @return string
+	**/
 	private function linkBridge($html, $pathToAssets = array('assets/', 'cached-assets/'), $pathToSite=SITE_URI){
 		
 		// ignore links that already have an address
@@ -194,6 +125,99 @@ class Core{
 		}
 		
 		return $newhtml;
+	}
+	
+	
+	/**
+	 * HtmlView: Sets path for controller
+	 * 
+	 * @param array $location
+	 *		order of directories and file to get the HTML content
+	 *		
+	 * @return boolean;
+	**/
+	protected function HtmlView(array $location){
+		
+		$path = dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.implode(DIRECTORY_SEPARATOR, $location).'.php';
+		
+		if(!file_exists($path)){
+			
+			header("Location: ".SITE_URI."/error/503");
+			exit;
+		}
+		
+		$this->HTMLPath = $path;
+		
+		return true;
+	}
+	
+	
+	/**
+	 * authorise: Grant Access manager
+	 * 
+	 * @param string $type
+	 *		default content to request
+	 *		
+	 * @return string $content
+	**/
+    protected function authorise(){
+        echo "content ".$this->var;
+                var_dump($_SESSION);
+		/* if($this->permission > $permissions){
+		
+			// si in plus daca $this->permission $this->userInput e post sau update atunci $this->getowner si s
+		} */
+    }
+    
+	/**
+	 * presentation: Framework Viewer
+	 * 
+	 * @param string $type
+	 *		default content to request
+	 *		
+	 * @return string $content
+	**/
+    public function presentation($type='html'){
+		
+		$content = '';
+		
+		// get content shipping method
+		if(isset($_GET['json']))
+			$_SESSION['shipment'] = 'json';
+		elseif(isset($_GET['xml']))
+			$_SESSION['shipment'] = 'xml';
+		elseif(!isset($_SESSION['shipment']) OR isset($_GET['html']))
+			$_SESSION['shipment'] = $type;
+		
+		if($_SESSION['shipment'] != 'html')
+			header("Content-Type: application/{$_SESSION['shipment']}; charset=utf-8");
+		
+		else{ // ENABLE COMPRESSION for HTML
+			
+			ob_start("ob_gzhandler");
+			include $this->HTMLPath;
+			$content = $this->linkBridge(ob_get_clean());
+			
+			// Determine supported compression method
+			$gzip = strstr($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip');
+			$deflate = strstr($_SERVER['HTTP_ACCEPT_ENCODING'], 'deflate');
+			
+			// Determine used compression method
+			$encoding = $gzip ? 'gzip' : ($deflate ? 'deflate' : false);
+
+			if($encoding){
+				$content = gzencode(trim( preg_replace( '/\s+/', ' ', $content ) ), 9);
+				$offset = 60 * 60 * 24;
+				$expire = "Expires: " . gmdate("D, d M Y H:i:s", time() + $offset) . " GMT";
+				header('Content-Encoding: gzip');
+				header("content-type: text/html; charset=UTF-8");
+				header( $expire );
+				header('Content-Length: ' . strlen( $content ) );
+				header('Vary: Accept-Encoding');
+			}
+		}
+		
+		print $content;
 	}
 	
 	
