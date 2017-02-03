fdb5adac91e61dddc5a464e24d6361ff53c3a4f1
AI Cylosh <ionuthrive@yahoo.com>
2017-02-03 23:34:10 +0200

    Update error 500

--- includes/500.php
+++ includes/500.php
@@ -1,69 +1,44 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-<title>oops Not Found</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-<link href='http://fonts.googleapis.com/css?family=Love+Ya+Like+A+Sister' rel='stylesheet' type='text/css'>
-<style type="text/css">
-body{
-	font-family: 'Love Ya Like A Sister', cursive;
-}
-body{
-	background:#eaeaea;
-}	
-.wrap{
-	margin:0 auto;
-	width:1000px;
-}
-.logo{
-	text-align:center;
-	margin-top:200px;
-}
-.logo img{
-	width:350px;
-}
-.logo p{
-	color:#272727;
-	font-size:40px;
-	margin-top:1px;
-}	
-.logo p span{
-	color:lightgreen;
-}	
-.sub a{
-	color:#fff;
-	background:#272727;
-	text-decoration:none;
-	padding:10px 20px;
-	font-size:13px;
-	font-family: arial, serif;
-	font-weight:bold;
-	-webkit-border-radius:.5em;
-	-moz-border-radius:.5em;
-	-border-radius:.5em;
-}	
-.footer{
-	color:black;
-	position:absolute;
-	right:10px;
-	bottom:10px;
-}	
-.footer a{
-	color:rgb(114, 173, 38);
-}	
-</style>
-</head>
 
-
+<html><head><title>500 Internal Server Error</title></head>
 <body>
- <div class="wrap">
-	<div class="logo">
-			<p>OOPS! - Could not Find it</p>
-			<img src="<?php if(defined("SITE_URI")) echo SITE_URI; ?>/assets/images/404.png"/>
-			<div class="sub">
-			  <p><a href="javascript:window.history.back();">Back</a></p>
-			</div>
-	</div>
- </div>	
-	
+<h1>500 Internal Server Error</h1>
+<p>Sorry, something went wrong.<br><br>A team of highly trained monkeys has been dispatched to deal with this situation.</p>
+<div style="min-width:800px;position:relative"><svg viewBox="0 0 490 525" style="width:350;float:right"><path fill="#6A1B9A" d="M325 85c1 12-1 25-5 38-8 29-31 52-60 61-26 8-54 14-81 18-37 6-26-37-38-72l-4-4c0-17-9-33 4-37l33-4c9-2 9-21 11-30 1-7 3-14 5-21 8-28 40-42 68-29 18 9 36 19 50 32 13 11 16 31 17 48z"/><path fill="none" stroke="#6A1B9A" stroke-width="24" stroke-linecap="round" stroke-miterlimit="10" d="M431 232c3 15 21 19 34 11 15-9 14-30 5-43-12-17-38-25-59-10-23 18-27 53-21 97s1 92-63 108"/><path fill="#6A1B9A" d="M284 158c35 40 63 85 86 133 24 52-6 113-62 123-2 0-4 1-6 1-53 9-101-33-101-87V188l83-30z"/><path fill="#F7CB4D" d="M95 152c-3-24 13-57 46-64l27-5c9-2 16-19 17-28l3-15 20-3c44 14 42 55 18 69 22 0 39 26 32 53-5 18-20 32-39 36-13 3-26 5-40 8-50 8-80-14-84-51z"/><path fill="#6A1B9A" d="M367 392c-21 18-77 70-25 119h-61c-27-29-32-69 1-111l85-8z"/><path fill="#6A1B9A" d="M289 399c-21 18-84 62-32 111h-61c-37-34-5-104 43-134l50 23z"/><path fill="#EDB526" d="M185 56l3-15 20-3c25 8 35 25 35 41-12-18-49-29-58-23z"/><path fill="#E62117" d="M190 34c8-28 40-42 68-29 18 9 36 19 50 32 10 9 14 23 16 37L187 46l3-12z"/><path fill="#8E24AA" d="M292 168c0 0 0 201 0 241s20 98 91 85l-16-54c-22 12-31-17-31-37 0-20 0-108 0-137S325 200 292 168z"/><path fill="#F7CB4D" d="M284 79c11-9 23-17 35-23 25-12 54 7 59 38v1c4 27-13 51-36 53-12 1-25 1-37 0-22-1-39-27-32-52v-1c2-6 6-12 11-16z"/><path fill="#6A1B9A" d="M201 203s0 84-95 140l22 42s67-25 89-86-16-96-16-96z"/><path fill="#BE2117" d="M224 54l-67-14c-10-2-13-15-5-21s18-6 26 0l46 35z"/><circle fill="#4A148C" cx="129" cy="161" r="12"/><circle fill="#4A148C" cx="212" cy="83" r="7"/><circle fill="#4A148C" cx="189" cy="79" r="7"/><path fill="#F7CB4D" d="M383 493c11-3 19-8 25-13 7-10 4-16-5-20 8-9 2-22-8-18 1-1 1-2 1-3 3-9-9-15-15-8-3 4-8 7-13 9l15 53z"/><path fill="#EDB526" d="M252 510c5 6 0 15-9 15h-87c-10 0-16-8-13-15 5-12 21-19 36-16l73 16z"/><ellipse transform="rotate(19.126 278.35 14.787)" fill="#E62117" cx="278" cy="15" rx="9" ry="7"/><path fill="#F7CB4D" d="M341 510c5 6 0 15-9 15h-87c-10 0-16-8-13-15 5-12 21-19 36-16l73 16z"/><path fill="#EDB526" d="M357 90c-12-19-35-23-55-11-19 12-25 32-13 52"/><path fill="#E62117" d="M110 427l21-9c5-2 7-8 5-13l-42-94c-3-6-9-9-15-6l-11 5c-6 2-9 9-7 15l36 97c2 5 8 7 13 5z"/><path fill="#B0BEC5" d="M37 278l41-17c11-4 22-5 33-1 5 2 10 4 14 6 6 3 4 11-3 11-9 0-18 1-26 3l2 12c1 6-2 11-8 13l-36 15c-5 2-10 1-14-2l-9-7-2 17c0 2-2 4-4 5l-3 1c-3 1-7 0-8-3L1 300c-1-3 0-7 4-9l4-2c2-1 5 0 7 1l12 10 1-11c0-5 3-9 8-11z"/><path fill="#F7CB4D" d="M103 373c10 2 14 10 8 19 6-1 10 4 10 9 0 3-3 6-6 7l-26 11c-2 1-5 1-8 0-6-3-7-9-2-16-7-1-13-9-6-17-8-1-12-8-8-15l3-3 23-11c9-4 19 8 12 16z"/><ellipse transform="rotate(173.3 233.455 334.51)" fill="#8E24AA" cx="234" cy="335" rx="32" ry="46"/></svg></div>
+
+If you see them, send them this information as text (screenshots frighten them):<br>
+<pre>AB38WEOSh7-8cMo47ad2eXgqPoUCBRoJo9hzCi8r05wUSPUzX2I1zGP9
+    82PB0tJBz6NfEE78pcG-FqTNp36b7iPJyHbOkcNmV9DpAAr74Xs3BZE9
+    yxBMIsmK_FTuScO3kAJrBLq9x2iwNyPoaPO5l1ZLaPFHbf4tGTV8DNZI
+    HvJhdtuRTuM_LQGdDmOxa14tDPlpJ8yh-_8jg-ID-pQupqhDsdtOlHdn
+    i9OTYQLZx8F-dKxpJgpe9srA55BTlqYVl9Dh8-hrvkGPV6O05oklKjyl
+    bqFQoXKJPd_IInIz4mOUssz9lt_tjfVCZ2_eNSvEahcEd3sUBLDHJzfM
+    nPLA5_UjcHOZJ0iuoKvk6vsy6X29j4dlLHVJtCTmq51YWk0yLVAmfwZY
+    9bLZtHECggIEki9mW6XrJpRa7uwBBKPUS0ohBT-zp3_bMjAEHYjX9Xw1
+    VaIcatsT5beGAFxd_GYj5KCUIdQmiNykJEvWeyUiwukonM-RhnZpM3F0
+    YFWYuv59M7nE8BO7J4ekF1gx1pd2fUgQml0_bIBnQGxJdEFs0_-gjkpL
+    tenhNS7so01CZXj6_3E7gYbkHZv_TO1Yjw4hDXbi_VqGYKfcWXPylisH
+    mGOAAW9gONfy8kiEgmOu6n0bOCrGrLR0t_tXfsee000sKhRws-pqXXk-
+    Nemhqfimcm9jbZWzMFl85YhDQTLJK1FScwdAX9gETKB3RleE2-uwZQ6n
+    -8IhmGHC1X5qnFBLgzQ2A4FfjQN_u4ftw0hkfTladcUmBes7G4acnp7F
+    ckGOCN1mnFjfrA7qcnUB4pCKBOnXhSkdtInUFv_WXO7PcIQxRy0riFyC
+    oXKjjMuIWPP3s93ICn97-mIo2Ngij4zHK7XwVfD8QIjMlLPATVWkHyPQ
+    XQEu86RPvJ3ohYhgDh0Hh19qg2LobxvcvuD5WEfexQx08gmW-RP1P2Ex
+    6vOgnW3DkZTCmgPwJX0AflxeYJc4-TrR-FMpzZ-Ej6quYqf2HpqqaIhN
+    UOPalkPUmQFcnmySPX2LLFLDbdGzXpysV4TfkD-jvLIwUhn3HHsv_p_D
+    lHkk6w0sK5bMG8H232qbeTBwYA7Tpi16DQ5ET6bluwpASCnomqe2yT-p
+    Fcn4bLmrQsha8zx0vFm4byPvixWxxheolldQE-j2qENtGKcvS9wUwiBV
+    A9prqd0pDheBVKlFfolXDvL8lt-Ql-qZv6ixgRKQRQydq6T8jo3Kjn4R
+    -Qeervu1VYVa-JA-oW2KXfasVd89flQNszwFve4NiiJltrzdqibLw4Kx
+    Mee1XAU1Lx2GWAhNbojBzZGNzcP3KZ93ve6tGNzUIWsFY9VZkW_DYp2d
+    03hDHmziuMkUO9ioviEplcA93WpVlzjTtJ3DbFga-22-_rVpAWj18eCT
+    RX8qbd-5xyXa7-_LJErQtNsBBeQpKzWH01ypm7kTTXckdvi1cEjPvTu_
+    1jiO3y8AuH3o5ADPAkxLrFrCoO6zPieSloPR9PzaaLuYtcrRfPhgD8CQ
+    jVAqnQAUPbdKfVFs_ovRV3YNHK49GO6ELZ3FaLqC7UBAs8uE9oUucd5J
+    iqT8vcI9bKu2HABd65RBefQu6uLiQJYaESatKG7ZUn06e9Y1Y9gz2hpF
+    QOABATNhXxfA4T0JyaPHL2XSFkk1fqfFddWpukbpCmo1pgu17L7k6u_p
+    wMR_T3jBJsiXnoYyOXiDKQPObcjJ9iwMczLnidyk29qEVlffW9P1QZkW
+    K2o73tUdnFcXQLE_koDOnC4uYrbZ-ae6q20qDhtG3rPTy76PIuicqWxd
+    1cTmdLnkbdOGciDWpUfJByQaDn8h3v6oT3zJXOQYluB4ALkD1pnhWQYX
+    CP3sb4Mfo-Pk4phJKiTh4JAncE_euZNE-mK2mqQwocZIOG06WBRSvF-L</pre>
 </body>
+</html>
