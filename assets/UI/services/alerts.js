8769b2865b7eae2c54bd5296429bb540bcca1c74
AI Cylosh <ionuthrive@yahoo.com>
2017-02-08 23:36:49 +0200

    Update UI Alerts

--- assets/UI/services/alerts.js
+++ assets/UI/services/alerts.js
@@ -2,8 +2,9 @@ var Alerts = {
 	text: 'Error',
 	icoclass: 'glyphicon-exclamation-sign',
 	divclass: 'alert-danger',
+	HTML: '',
 	
-	buildHTML: function(){
+	buildDOM: function(){
 		
 		var alertDIV = document.createElement('div');
 		alertDIV.setAttribute('class', 'alert fade in '+ this.divclass);
@@ -15,21 +16,107 @@ var Alerts = {
 		
 		var alertMessage = document.createElement('span');
 		alertMessage.setAttribute('style', 'font-size:120%');
-		alertMessage.innerHTML = this.text;
+		alertMessage.innerHTML = '&nbsp;'+this.text;
 		
 		alertDIV.appendChild(alertICO);
 		alertDIV.appendChild(alertMessage);
 		
+		this.HTML += alertDIV.outerHTML;
+		
 		return alertDIV;
 	},
 	
-	warn: function(message){
-		this.text = '&nbsp;'+message+Math.random();
+	danger: function(){
 		this.icoclass = 'glyphicon-exclamation-sign';
 		this.divclass = 'alert-danger';
-		
-		return this.buildHTML();
-	}
+	},
+
+	warning: function(){
+		this.icoclass = 'glyphicon-warning-sign';
+		this.divclass = 'alert-warning';
+	},
+
+	success: function(){
+		this.icoclass = 'glyphicon-ok-sign';
+		this.divclass = 'alert-success';
+	},
+
+	info: function(){
+		this.icoclass = 'glyphicon-info-sign';
+		this.divclass = 'alert-info';
+	},
 	
+	setType: function(type){
+		switch(type){
+			case "alert":
+				this.danger();
+				break;
+			
+			case "warn":
+				this.warning();
+				break;
+			
+			case "good":
+				this.success();
+				break;
+			
+			default:
+				this.info();
+		}
+		
+	},
+	
+	parse: function(alerts){
+		
+		this.HTML = '';
+		var alertType = 'info';
+		
+		if(typeof alerts === "string"){
+			this.text = alerts;
+			this.setType(alertType);
+		}
+		
+		if(typeof alerts === "object"){
+			
+			//find all known type of alerts
+			if(alerts.error !== undefined){
+				inAlerts = alerts.error;
+				alertType = 'warn';
+			}else if(alerts.success !== undefined){
+				inAlerts = alerts.success;
+				alertType = 'good';
+			}else
+				return false;
+			
+			if(typeof inAlerts === "string"){
+				this.setType(alertType);
+				this.text = inAlerts;
+				this.buildDOM();
+			}else{
+				for(alertType in inAlerts){
+					if(typeof inAlerts === "object"){
+						
+						var alerTXT = inAlerts[alertType];
+						this.setType(alertType);
+						
+						if (typeof alerTXT === "string"){
+							this.text = alerTXT;
+							this.buildDOM();
+						}
+						
+						if(Array.isArray(alerTXT)){
+							for (i = 0; i < alerTXT.length; i++){
+								this.text = alerTXT[i];
+								this.buildDOM();
+							}
+						}
+						
+					}
+				}
+			}
+		}
+		
+		return this.HTML;
+	}
 }
 
