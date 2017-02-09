ab8cff6455caa72945b0e02054d488f725cc16d3
AI Cylosh <ionuthrive@yahoo.com>
2017-02-09 23:58:23 +0200

    Cleanup & avoid Ninja Comments for Contact JS

--- assets/UI/services/SendContact.js
+++ assets/UI/services/SendContact.js
@@ -10,9 +10,10 @@ function onSubmitContact() {
 		//this will prevent the page to reload when 
 		//user clicks submit button
 		ev.preventDefault();
-		
+		//remove previous alerts
 		$("div[class~='alert']").remove();
 		
+		//set inputs objects via jquery
 		var name = $("input[name='name']");
 		var email = $("input[name='email']");
 		var phone = $("input[name='phone']");
@@ -25,38 +26,43 @@ function onSubmitContact() {
 		formData.append("phone",phone.val());
 		formData.append("message",message.val());
 		
+		// instantiate new contact object
 		var EM = new Contact({
 			name:name.val(),
 			email:email.val(),
 			message:message.val()
 		});
-		
+		//switch between loading and submit buttons
 		$("#newContact, #sendContact").toggle();
-
+		
+		// send request
 		jXHR = EM.send(formData);
-		jXHR.done(function(data, textStatus, resp) {
+		jXHR.done(function(data, textStatus, resp) {//process successful response
+			// show alerts
 			$('#contact').before($(Alerts.parse(resp.responseJSON)).fadeIn(150));
-			console.log(resp);
-			console.log(resp.responseJSON);
+			//remove alerts after 3 seconds
 			$("div[class~='alert']").delay(3000).fadeOut("slow");
-	
+			//clean the form inputs
 			$(name).val(''),$(email).val(''),$(phone).val(''),$(message).val('');
 		})
-		.fail(function(resp) {
+		.fail(function(resp) {//handle failed result
 			
+			//show alerts
 			$('#contact').before($(Alerts.parse(resp.responseJSON)).fadeIn(150));
-			
+			// remove alerts after 10 seconds
 			$("div[class~='alert']").delay(10000).fadeOut("slow");
-
+			//disable submit button
 			$('#newContact').prop('disabled', true);
 			setTimeout(function ()
-			{
+			{//re-enable after 3 seconds
 				$('#newContact').prop('disabled', false);
 			}, 3000);
 			
 		});
-		
+		//switch between loading and submit buttons
 		$("#newContact, #sendContact").toggle();
+		
+		//take client window to correct height in order for it to spot the alerts(in case of)
 		window.location.hash = 'send';
 		window.location.hash = 'form';
 
