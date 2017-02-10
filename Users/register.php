6d73c78d39306ef3705cc58f6ffc3cf58a3c7dc8
AI Cylosh <ionuthrive@yahoo.com>
2017-02-10 21:12:01 +0200

    Build Register controller/model

--- Users/register.php
+++ Users/register.php
@@ -19,7 +19,7 @@ define("HEADER_SUBTITLE", 'For Cylosh Services');
 	<div id="register-alt">
 		<div class="container">
 			<div class="box-register">
-				<form action="register">
+				<form action="register/create">
 					<div class="text-center">
 						<h3>Register a new account</h3>
 						<small>Already Signed Up? Click <a href="login">Sign In</a> to login your account.</small>
@@ -54,7 +54,7 @@ define("HEADER_SUBTITLE", 'For Cylosh Services');
 					    </label>
 					</div>
 					<div class="form-group">
-						<button class="btn btn-warning btn-block">LOGIN</button>
+						<button class="btn btn-warning btn-block">REGISTER</button>
 					</div>
 				</form>
 			</div>
--- system/Controllers/Register.php
+++ system/Controllers/Register.php
@@ -9,9 +9,12 @@ class Register extends Core{
     
 	public static $defaultMethod = 'signup';
     function __construct(){
+		// check if already logged in
+		if(isset($_SESSION['is_user']))
+			return $this->Response['redirect'] = array("message"=>"Already logged in", "url"=>'/');
     }
     
-    public function signup(){
+    public function create(){
         return $this->HtmlView(array("Users", "register"));
     }
     
